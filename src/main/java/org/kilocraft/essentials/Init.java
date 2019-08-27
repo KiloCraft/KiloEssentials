7b5f771f6f1ed56475d993db9b405279a7be3ca5
MCrafterzz <marcusaccounts@yahoo.se>
2019-08-27 17:24:35 +0200

    Accualy call the command registration

--- src/main/java/org/kilocraft/essentials/Init.java
+++ src/main/java/org/kilocraft/essentials/Init.java
@@ -3,8 +3,12 @@ package org.kilocraft.essentials;
 import net.fabricmc.api.ModInitializer;
 
 public class Init implements ModInitializer {
+	
+	KiloCommands kiloCommands;
+	
     @Override
     public void onInitialize() {
         KiloEssentials.getLogger.info("KiloEssentials is loading...");
+        kiloCommands = new KiloCommands();
     }
 }
--- src/main/java/org/kilocraft/essentials/KiloCommands.java
+++ src/main/java/org/kilocraft/essentials/KiloCommands.java
@@ -1,14 +1,13 @@
 package org.kilocraft.essentials;
 
-import net.fabricmc.fabric.api.registry.CommandRegistry;
 import org.kilocraft.essentials.commands.DiscordCommands.DiscordCommand;
 import org.kilocraft.essentials.commands.VersionCommand;
+
+import net.fabricmc.fabric.api.registry.CommandRegistry;
 
 public class KiloCommands {
     public KiloCommands() {
         CommandRegistry.INSTANCE.register(true, VersionCommand::register);
         CommandRegistry.INSTANCE.register(true, DiscordCommand::register);
-
-
     }
 }
