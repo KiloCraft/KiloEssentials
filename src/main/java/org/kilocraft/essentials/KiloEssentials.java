fee71f89475cb7abc1f0c90060819ef5d9a2696e
MCrafterzz <marcusaccounts@yahoo.se>
2019-08-29 08:01:50 +0200

    Fixed mod not working

--- src/main/java/org/kilocraft/essentials/KiloEssentials.java
+++ src/main/java/org/kilocraft/essentials/KiloEssentials.java
@@ -1,17 +1,17 @@
 package org.kilocraft.essentials;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kilocraft.essentials.commands.KiloCommands;
 import org.kilocraft.essentials.config.ConfigHandler;
 
 
-public class KiloEssentials implements ModInitializer {
+public class KiloEssentials implements DedicatedServerModInitializer {
 	public static Logger getLogger = LogManager.getFormatterLogger("KiloEssentials");
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeServer() {
 		Mod mod = new Mod();
 		getLogger.info("Loading KiloEssentials...");
 		KiloCommands.register();
