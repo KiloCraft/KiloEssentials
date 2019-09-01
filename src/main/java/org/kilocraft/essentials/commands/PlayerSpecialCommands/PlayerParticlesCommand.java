0515b494409a162eeec1850345f026360b572978
MCrafterzz <marcusaccounts@yahoo.se>
2019-09-01 09:44:04 +0200

    Updated my commands messages to the new format

--- src/main/java/org/kilocraft/essentials/commands/PlayerSpecialCommands/PlayerParticlesCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/PlayerSpecialCommands/PlayerParticlesCommand.java
@@ -1,45 +1,37 @@
 package org.kilocraft.essentials.commands.PlayerSpecialCommands;
 
-import org.kilocraft.essentials.Mod;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Style;
-import net.minecraft.util.Formatting;
 import org.kilocraft.essentials.commands.CommandArguments.PlayerParticlesCommandArgument;
+import org.kilocraft.essentials.utils.LangText;
 
 public class PlayerParticlesCommand {
 
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(CommandManager.literal("particles").executes(context -> {
-			context.getSource()
-					.sendFeedback(new LiteralText(Mod.lang.getProperty("command.playerparticles.onlyoneargument"))
-							.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.playerparticles.onlyoneargument"),
+					false);
 			return 1;
 		}).then(CommandManager.literal("set").executes(context -> {
-			context.getSource()
-					.sendFeedback(new LiteralText(Mod.lang.getProperty("command.playerparticles.noparticleschosen"))
-							.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.playerparticles.noparticleschosen"),
+					false);
 			return 1;
 		}).then(CommandManager.argument("name", PlayerParticlesCommandArgument.particles()).executes(context -> {
 			// TODO: Change particle
 			String particle = PlayerParticlesCommandArgument.getParticleName(context, "name");
 			if (PlayerParticlesCommandArgument.NAMES.contains(particle)) {
-				context.getSource().sendFeedback(
-						new LiteralText(Mod.lang.getProperty("command.playerparticles.particleset")), false);
+				context.getSource().sendFeedback(LangText.getFormatter(true, "command.playerparticles.particleset"),
+						false);
 				return 0;
 			} else {
-				context.getSource().sendFeedback(
-						new LiteralText(Mod.lang.getProperty("command.playerparticles.incorrectparticle"))
-								.setStyle(new Style().setColor(Formatting.RED)),
-						false);
+				context.getSource()
+						.sendFeedback(LangText.getFormatter(true, "command.playerparticles.incorrectparticle"), false);
 				return 1;
 			}
 		}))).then(CommandManager.literal("disable").executes(context -> {
 			// TODO: Disable particles
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.playerparticles.disable")),
-					false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.playerparticles.disable"), false);
 			return 0;
 		})));
 	}
--- src/main/java/org/kilocraft/essentials/commands/RankCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/RankCommand.java
@@ -1,6 +1,7 @@
 package org.kilocraft.essentials.commands;
 
 import org.kilocraft.essentials.Mod;
+import org.kilocraft.essentials.utils.LangText;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
@@ -8,64 +9,54 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import net.minecraft.command.arguments.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Style;
-import net.minecraft.util.Formatting;
 
 public class RankCommand {
 
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(CommandManager.literal("rank").executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.onlyoneargument"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.onlyoneargument"), false);
 			return 1;
 		}).then(CommandManager.literal("add").executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noranktoadd"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noranktoadd"), false);
 			return 1;
 		}).then(CommandManager.argument("name", StringArgumentType.string()).executes(context -> {
 			// TODO: Add rank
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.addrank")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.addrank"), false);
 			return 0;
 		}))).then(CommandManager.literal("remove").executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noranktoremove"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noranktoremove"), false);
 			return 1;
 		}).then(CommandManager.argument("name", StringArgumentType.string()).executes(context -> {
 			// TODO: Remove rank
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.removerank")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.removerank"), false);
 			return 0;
 		}))).then(CommandManager.literal("join").executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noplayertojoin"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noplayertojoin"), false);
 			return 1;
 		}).then(CommandManager.argument("player", EntityArgumentType.players()).executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noranktojoin"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noranktojoin"), false);
 			return 1;
 		}).then(CommandManager.argument("name", StringArgumentType.string()).executes(context -> {
 			// TODO: Join rank
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.joinrank")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.joinrank"), false);
 			return 0;
 		})))).then(CommandManager.literal("leave").executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noplayertoleave"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noplayertoleave"), false);
 			return 1;
 		}).then(CommandManager.argument("player", EntityArgumentType.players()).executes(context -> {
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.noranktoleave"))
-					.setStyle(new Style().setColor(Formatting.RED)), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.noranktoleave"), false);
 			return 1;
 		}).then(CommandManager.argument("name", StringArgumentType.string()).executes(context -> {
 			// TODO: Join rank
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.leaverank")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.leaverank"), false);
 			return 0;
 		})))).then(CommandManager.literal("list").executes(context -> {
 			// TODO: List ranks
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.list")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.list"), false);
 			return 0;
 		}).then(CommandManager.argument("player", EntityArgumentType.players()).executes(context -> {
 			// TODO: List player ranks
-			context.getSource().sendFeedback(new LiteralText(Mod.lang.getProperty("command.rank.list.player")), false);
+			context.getSource().sendFeedback(LangText.getFormatter(true, "command.rank.list.player"), false);
 			return 0;
 		}))));
 	}
