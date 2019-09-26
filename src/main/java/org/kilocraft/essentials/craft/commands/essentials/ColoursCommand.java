d25467e2d8b66fa2b4906205a6c1147ddf7da627
MCrafterzz <marcusaccounts@yahoo.se>
2019-09-26 16:59:15 +0200

    Changed /colours message to white

--- src/main/java/org/kilocraft/essentials/craft/commands/essentials/ColoursCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/ColoursCommand.java
@@ -20,7 +20,7 @@ public class ColoursCommand {
 
 	public static int execute(CommandContext<ServerCommandSource> context) {
 		LiteralText text = new LiteralText(
-				"§8§lColour codes:§r\n§0&0Black §1&1Dark Blue §2&2Dark Green §3&3Dark Aqua §4&4Dark Red §5&5Dark Purple §6&6Gold §7&7Gray §8&8Dark Gray §9&9Blue §a&aGreen §b&bAqua §c&cRed §d&dPurple §e&eYellow §f&fWhite\n\n§8§lFormatting codes:\n§r§8§l&lBold §r§8§o&oItalic §r§8§n&nUnderline §r§8§m&mStrikethrough §r§8&kObfuscated§kexample §8&rReset To Default\n"
+				"§f§lColour codes:§r\n§0&0Black §1&1Dark Blue §2&2Dark Green §3&3Dark Aqua §4&4Dark Red §5&5Dark Purple §6&6Gold §7&7Gray §8&8Dark Gray §9&9Blue §a&aGreen §b&bAqua §c&cRed §d&dPurple §e&eYellow §f&fWhite\n\n§f§lFormatting codes:\n§r§f§l&lBold §r§f§o&oItalic §r§f§n&nUnderline §r§f§m&mStrikethrough §r§f&kObfuscated§kexample §f&rReset To Default\n"
 						+ "");
 		context.getSource().sendFeedback(text, false);
 		return 0;
