e9795d2e633d2599019b1029fcc02f197bad2fb2
MCrafterzz <marcusaccounts@yahoo.se>
2019-09-26 16:50:23 +0200

    Colours command

---
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/ColoursCommand.java
@@ -0,0 +1,28 @@
+package org.kilocraft.essentials.craft.commands.essentials;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
+
+public class ColoursCommand {
+	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+		dispatcher.register(CommandManager.literal("colours").executes(context -> {
+			return execute(context);
+		}));
+
+		dispatcher.register(CommandManager.literal("colors").executes(context -> {
+			return execute(context);
+		}));
+	}
+
+	public static int execute(CommandContext<ServerCommandSource> context) {
+		LiteralText text = new LiteralText(
+				"§l§8Colour codes:§r\n§0&0Black §1&1Dark Blue §2&2Dark Green §3&3Dark Aqua §4&4Dark Red §c&cDark Purple §6&6Gold §7&7Gray §8&8Dark Gray §9&9Blue §a&aGreen §b&bAqua §c&cRed §d&dPurple §e&eYellow §f&fWhite\n\n§l§8Formatting codes:\n§r§l§8&lBold §r§o§8&oItalic §r§n§8&nUnderline §r§m§8&mStrikethrough §r§k§8&kObfuscated §8&rReset To Default\n"
+						+ "");
+		context.getSource().sendFeedback(text, false);
+		return 0;
+	}
+}
