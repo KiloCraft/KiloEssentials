46def5804c897fda27f0c731c13056d1ea26141b
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-09-27 20:18:36 +0330

    Revert "Fixed homes(probably, didn't test)"
    This reverts commit 767b97120e906b966e7118c8deb7b90d89770f6d.

--- src/main/java/org/kilocraft/essentials/craft/homesystem/PlayerHomeManager.java
+++ src/main/java/org/kilocraft/essentials/craft/homesystem/PlayerHomeManager.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 
 public class PlayerHomeManager {
     NbtFile nbt = new NbtFile("/KiloEssentials/data/", "homes");
-    public static PlayerHomeManager INSTANCE = null;
+    protected static PlayerHomeManager INSTANCE = null;
     private HashMap<String, Home> hashMap = new HashMap<>();
 
     public void addHome(Home home) {
@@ -41,7 +41,7 @@ public class PlayerHomeManager {
         return tag;
     }
 
-    public void fromNbt(CompoundTag tag) {
+    public void fronNbt(CompoundTag tag) {
         hashMap.clear();
         tag.getKeys().forEach(key -> {
             ListTag playerTag = (ListTag) tag.getTag(key);
@@ -51,6 +51,13 @@ public class PlayerHomeManager {
                 hashMap.put(home.name, home);
             });
         });
+    }
+
+
+
+
+    public static PlayerHomeManager getInstance() {
+        return INSTANCE;
     }
 
     public HashMap<String, Home> getHashMap() {
