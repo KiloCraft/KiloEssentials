197819a192f5a468f4425a2ff27e0b9513e6193c
MCrafterzz <marcusaccounts@yahoo.se>
2019-10-07 08:54:53 +0200

    Cleaner implementation

--- src/main/java/org/kilocraft/essentials/api/mixin/event/MixinServerPlayerEntity$PlayerEvent$OnDeath.java
+++ src/main/java/org/kilocraft/essentials/api/mixin/event/MixinServerPlayerEntity$PlayerEvent$OnDeath.java
@@ -2,11 +2,9 @@ package org.kilocraft.essentials.api.mixin.event;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
 import org.kilocraft.essentials.api.KiloServer;
 import org.kilocraft.essentials.api.event.eventImpl.playerEventsImpl.PlayerEvent$OnDeathImpl;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,11 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class MixinServerPlayerEntity$PlayerEvent$OnDeath {
 	
-	@Shadow ServerPlayerInteractionManager interactionManager;
-	
     @Inject(at = @At("HEAD"), method = "onDeath")
     private void oky$death(DamageSource damageSource_1, CallbackInfo ci) {
-        KiloServer.getServer().triggerEvent(new PlayerEvent$OnDeathImpl(interactionManager.player));
+        KiloServer.getServer().triggerEvent(new PlayerEvent$OnDeathImpl((ServerPlayerEntity) (Object) this));
     }
 
 }
