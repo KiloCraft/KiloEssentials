4e2fd2303c1bf4ecf65d999694ecc1a3c8133100
MCrafterzz <marcusaccounts@yahoo.se>
2019-10-13 09:58:10 +0200

    Added random teleport (Testing required)

--- src/main/java/org/kilocraft/essentials/craft/KiloCommands.java
+++ src/main/java/org/kilocraft/essentials/craft/KiloCommands.java
@@ -60,6 +60,7 @@ public class KiloCommands {
         ColorsCommand.register(dispatcher);
         StopCommand.register(dispatcher);
         BackCommand.register(dispatcher);
+        RandomTeleportCommand.register(dispatcher);
     }
 
     public static String buildSmartUsage(LiteralCommandNode<ServerCommandSource> literalCommandNode, ServerCommandSource source) {
