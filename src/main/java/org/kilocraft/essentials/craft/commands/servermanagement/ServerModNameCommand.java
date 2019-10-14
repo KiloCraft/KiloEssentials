401e65ae542912c8bfd10b1a29bd495b2fdf4b74
MCrafterzz <marcusaccounts@yahoo.se>
2019-10-14 16:20:53 +0200

    Added permissions for servermodname command

--- src/main/java/org/kilocraft/essentials/craft/commands/servermanagement/ServerModNameCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/servermanagement/ServerModNameCommand.java
@@ -3,6 +3,8 @@ package org.kilocraft.essentials.craft.commands.servermanagement;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+import io.github.indicode.fabric.permissions.Thimble;
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
 import net.minecraft.server.command.CommandManager;
@@ -16,7 +18,7 @@ import org.kilocraft.essentials.api.chat.TextColor;
 public class ServerModNameCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> literalArgumentBuilder = CommandManager.literal("server")
-                .requires(source -> source.hasPermissionLevel(4))
+                .requires(s -> Thimble.hasPermissionChildOrOp(s, "kiloessentials.command.servermodname", 2))
                 .then(CommandManager.literal("config").then(CommandManager.literal("brandName")
                         .then(CommandManager.argument("name", StringArgumentType.greedyString())
                                 .executes(context -> execute(context.getSource(), StringArgumentType.getString(context, "name"))))));
