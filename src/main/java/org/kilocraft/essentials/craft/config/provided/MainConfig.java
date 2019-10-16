47f088d41c73e7a1e4f96fdb5cbf16aabf407c6c
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-10-16 22:09:26 +0330

    Working on ConfigProvided

--- src/main/java/org/kilocraft/essentials/craft/config/provided/MainConfig.java
+++ src/main/java/org/kilocraft/essentials/craft/config/provided/MainConfig.java
@@ -1,17 +1,29 @@
 package org.kilocraft.essentials.craft.config.provided;
 
+import com.electronwill.nightconfig.core.file.FileConfig;
+import org.kilocraft.essentials.craft.config.KiloConifg;
+
 public class MainConfig {
     public MainConfig() {
-
+        provide();
     }
 
     /**
      * Server
      */
+    private char altColorChar;
     private String name;
     private String motd;
     private String playerCountMessage;
 
+    private FileConfig config = KiloConifg.getMain();
 
+    private void provide() {
+        name = config.getOrElse("server.name", "Minecraft server");
+        motd = config.getOrElse("server.motd", "NULL");
+        playerCountMessage = config.getOrElse("server.playercountmessage", "NULL");
+
+
+    }
 
 }
