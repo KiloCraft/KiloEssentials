7cc62e3de4ce22797f5bed00f0a45bf5c0d8bf50
Indigo Amann <indigoa.coding@gmail.com>
2019-10-19 10:43:36 -0700

    Fix locate perms

--- src/main/java/org/kilocraft/essentials/craft/commands/essentials/locateCommands/LocateCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/locateCommands/LocateCommand.java
@@ -5,10 +5,11 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.github.indicode.fabric.permissions.Thimble;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import org.kilocraft.essentials.craft.KiloCommands;
 
 public class LocateCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-
+        KiloCommands.getCommandPermission("locate");
         LiteralArgumentBuilder<ServerCommandSource> builder = CommandManager.literal("ke_locate")
                 .requires(s -> Thimble.hasPermissionChildOrOp(s, "kiloessentials.command.locate", 2));
 
