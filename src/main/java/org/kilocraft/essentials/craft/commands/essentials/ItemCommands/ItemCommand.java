3aeee669752bfa8225a93c5928a005ca96238cc9
Indigo Amann <indigoa.coding@gmail.com>
2019-10-19 10:45:20 -0700

    Fix item perms

--- src/main/java/org/kilocraft/essentials/craft/commands/essentials/ItemCommands/ItemCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/ItemCommands/ItemCommand.java
@@ -6,11 +6,13 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.github.indicode.fabric.permissions.Thimble;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import org.kilocraft.essentials.craft.KiloCommands;
 
 public class ItemCommand {
     private static LiteralArgumentBuilder<ServerCommandSource> argumentBuilder = CommandManager.literal("item");
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        KiloCommands.getCommandPermission("item");
     	argumentBuilder.requires(s -> Thimble.hasPermissionChildOrOp(s, "kiloessentials.command.item", 2));
     	
         ItemNameCommand.registerChild(argumentBuilder);
