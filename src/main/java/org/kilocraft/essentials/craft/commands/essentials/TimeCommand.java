e03a0a7e0f18b67d2d8fbac3fe56ed37e77c43dd
DrexHD <nicknamedrex@gmail.com>
2019-10-22 20:23:36 +0200

    Create TimeCommand.java

---
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/TimeCommand.java
@@ -0,0 +1,47 @@
+package org.kilocraft.essentials.craft.commands.essentials;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import io.github.indicode.fabric.permissions.Thimble;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import org.kilocraft.essentials.craft.KiloCommands;
+import java.util.Iterator;
+
+public class TimeCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
+
+        LiteralArgumentBuilder<ServerCommandSource> time = CommandManager.literal("test").requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("time"),2));
+        RequiredArgumentBuilder<ServerCommandSource, String> argSet;
+        argSet = CommandManager.argument("set", StringArgumentType.string()).requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("time"),2));
+
+        dispatcher.register(time);
+        time.then(argSet);
+
+        argSet.executes(c -> executeSet(c));
+
+                argSet.then(CommandManager.argument("ticks", IntegerArgumentType.integer(1))
+                        .requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("time.set"),2))
+                        .executes(c -> executeSet(c)));
+    }
+
+    public static int executeSet(CommandContext<ServerCommandSource> context){
+        int arg = IntegerArgumentType.getInteger(context, "ticks");
+        Iterator world = context.getSource().getMinecraftServer().getWorlds().iterator();
+        while(world.hasNext()){
+            ServerWorld kcworld = (ServerWorld) world.next();
+            kcworld.setTime((long) arg);
+        }
+        return 1;
+    }
+    public static int executeGet(CommandContext<ServerCommandSource> context){
+        return (int)(context.getSource().getWorld().getTimeOfDay() % 24000L);
+    }
+
+}
