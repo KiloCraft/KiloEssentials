f3f9618702bd1c8ee074710a48fe4ba2a3841ade
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-10-23 05:59:53 +0330

    Updated the FlyCommand

--- src/main/java/org/kilocraft/essentials/craft/commands/essentials/FlyCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/FlyCommand.java
@@ -3,18 +3,14 @@ package org.kilocraft.essentials.craft.commands.essentials;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.indicode.fabric.permissions.Thimble;
-import net.minecraft.command.EntitySelector;
 import net.minecraft.command.arguments.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.kilocraft.essentials.api.chat.TextFormat;
+import org.kilocraft.essentials.api.util.CommandHelper;
 import org.kilocraft.essentials.api.util.CommandSuggestions;
 import org.kilocraft.essentials.craft.KiloCommands;
-import org.kilocraft.essentials.craft.chat.ChatMessage;
 import org.kilocraft.essentials.craft.chat.KiloChat;
 
 public class FlyCommand {
@@ -22,37 +18,34 @@ public class FlyCommand {
         KiloCommands.getCommandPermission("fly");
         LiteralArgumentBuilder<ServerCommandSource> argumentBuilder = CommandManager.literal("fly")
                 .requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("fly"), 2))
-                .executes(c -> toggle(c.getSource(), c.getSource().getPlayer()));
-        RequiredArgumentBuilder<ServerCommandSource, EntitySelector> selectorArg = CommandManager.argument("player", EntityArgumentType.player())
-                .requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("fly.others"), 2))
-                .suggests((context, builder) -> CommandSuggestions.allPlayers.getSuggestions(context, builder))
-                .executes(c -> toggle(c.getSource(), EntityArgumentType.getPlayer(c, "player")));;
-        RequiredArgumentBuilder<ServerCommandSource, Boolean> boolArg = CommandManager.argument("set", BoolArgumentType.bool())
-                .executes(c -> execute(c.getSource(), EntityArgumentType.getPlayer(c, "player"), BoolArgumentType.getBool(c, "set")));
-
-        selectorArg.then(boolArg);
-        argumentBuilder.then(selectorArg);
+                .executes(c -> toggle(c.getSource(), c.getSource().getPlayer()))
+                .then(
+                        CommandManager.argument("player", EntityArgumentType.player())
+                                .requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("fly.others"), 2))
+                                .suggests((context, builder) -> CommandSuggestions.allPlayers.getSuggestions(context, builder))
+                                .executes(c -> toggle(c.getSource(), EntityArgumentType.getPlayer(c, "player")))
+                                .then(
+                                        CommandManager.argument("set", BoolArgumentType.bool())
+                                                .executes(c -> execute(c.getSource(), EntityArgumentType.getPlayer(c, "player"), BoolArgumentType.getBool(c, "set")))
+                                )
+                );
 
         dispatcher.register(argumentBuilder);
     }
 
-    private static int toggle(ServerCommandSource source, ServerPlayerEntity playerEntity) throws CommandSyntaxException {
+    private static int toggle(ServerCommandSource source, ServerPlayerEntity playerEntity) {
         execute(source, playerEntity, !playerEntity.abilities.allowFlying);
         return 1;
     }
 
-    private static int execute(ServerCommandSource source, ServerPlayerEntity playerEntity, boolean bool) throws CommandSyntaxException {
+    private static int execute(ServerCommandSource source, ServerPlayerEntity playerEntity, boolean bool) {
         playerEntity.abilities.allowFlying = bool;
-        playerEntity.abilities.flying = bool;
         playerEntity.sendAbilitiesUpdate();
 
-        String text = String.format("&eYou have set the fly ability to &b%s&e for &6%s&e.", playerEntity.abilities.allowFlying, playerEntity.getName().asString());
-        TextFormat.sendToUniversalSource(source, text, false);
+        KiloChat.sendLangMessageTo(source, "commands.general.set", "Flight", bool, playerEntity.getName().asString());
 
-        if (!source.getPlayer().getName().equals(playerEntity.getName())) {
-            String s = String.format("&e&a%s set the fly ability to &b%s for you!", source.getName(), playerEntity.abilities.allowFlying);
-            KiloChat.sendMessageTo(playerEntity, new ChatMessage(s, true));
-        }
+        if (!CommandHelper.areTheSame(source, playerEntity))
+            KiloChat.sendLangMessageTo(playerEntity, "commands.general.announce", source.getName(), "Flight", bool);
 
         return 1;
     }
