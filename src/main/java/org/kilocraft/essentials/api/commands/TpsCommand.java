46c66c2152b6303c31fd538665f6aae3b67a29c8
DrexHD <nicknamedrex@gmail.com>
2019-10-29 09:24:02 +0100

    Add colors to /tps

--- src/main/java/org/kilocraft/essentials/api/commands/TpsCommand.java
+++ src/main/java/org/kilocraft/essentials/api/commands/TpsCommand.java
@@ -21,13 +21,27 @@ public class TpsCommand {
         TextFormat.sendToUniversalSource(
                 context.getSource(),
                 String.format(
-                        "&6tps &8(&71m&8/&75m&8/&715m&8)&d %s&8,&d %s&8,&d %s&r",
+                        "&6TPS &8(&71m&8/&75m&8/&715m&8)&%s %s&8,&%s %s&8,&%s %s&r",
+                        tpstoColorCode(SomeGlobals.tps1.getAverage()),
                         SomeGlobals.tps1.getShortAverage(),
+                        tpstoColorCode(SomeGlobals.tps5.getAverage()),
                         SomeGlobals.tps5.getShortAverage(),
+                        tpstoColorCode(SomeGlobals.tps15.getAverage()),
                         SomeGlobals.tps15.getShortAverage()
                 ),
                 false);
 
         return 1;
     }
+
+    public static char tpstoColorCode(double tps){
+        if(tps > 15){
+            return 'a';
+        }else if(tps > 10){
+            return 'e';
+        }else{
+            return 'c';
+        }
+    }
+
 }
