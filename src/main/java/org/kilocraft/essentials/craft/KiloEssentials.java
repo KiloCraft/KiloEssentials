3677f83aa043dfc614ac27edd288125d284daeb9
Indigo Amann <indigoa.coding@gmail.com>
2019-10-30 14:37:32 -0700

    Fix warp manager

--- src/main/java/org/kilocraft/essentials/craft/KiloEssentials.java
+++ src/main/java/org/kilocraft/essentials/craft/KiloEssentials.java
@@ -1,7 +1,6 @@
 package org.kilocraft.essentials.craft;
 
 import io.github.indicode.fabric.permissions.Thimble;
-import io.github.indicode.fabric.permissions.command.CommandPermission;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kilocraft.essentials.api.Mod;
