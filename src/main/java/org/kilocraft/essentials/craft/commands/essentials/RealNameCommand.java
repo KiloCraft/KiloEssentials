eae959b2083eecdd0e82855052a682048973602c
MCrafterzz <marcusaccounts@yahoo.se>
2019-11-01 12:56:51 +0100

    Realname finished

--- src/main/java/org/kilocraft/essentials/craft/commands/essentials/RealNameCommand.java
+++ src/main/java/org/kilocraft/essentials/craft/commands/essentials/RealNameCommand.java
@@ -1,10 +1,8 @@
 package org.kilocraft.essentials.craft.commands.essentials;
 
 import org.kilocraft.essentials.api.chat.LangText;
-
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
-
 import io.github.indicode.fabric.permissions.Thimble;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
@@ -18,14 +16,12 @@ public class RealNameCommand {
 		KiloCommands.getCommandPermission("realname");
 		dispatcher.register(CommandManager.literal("realname")
 				.requires(s -> Thimble.hasPermissionOrOp(s, KiloCommands.getCommandPermission("realname"), 2))
-				.then(CommandManager.argument("target", StringArgumentType.string())).executes(context -> {
+				.then(CommandManager.argument("target", StringArgumentType.string()).executes(context -> {
 					String input = StringArgumentType.getString(context, "target");
-
 					for (int i = 0; i < context.getSource().getWorld().getPlayers().size(); i++) {
 						PlayerEntity player = context.getSource().getWorld().getPlayers().get(i);
 						User user = UserManager.getUser(player.getUuid());
-
-						if (input == user.getNickName() && input != "") {
+						if (input.equals(user.getNickName()) && input != "") {
 							context.getSource().getPlayer().sendMessage(LangText.getFormatter(true,
 									"command.realname.success", input, player.getName().asString()));
 							return 0;
@@ -36,6 +32,6 @@ public class RealNameCommand {
 							.sendMessage(LangText.getFormatter(true, "command.realname.error", input));
 
 					return 0;
-				}));
+				})));
 	}
 }
