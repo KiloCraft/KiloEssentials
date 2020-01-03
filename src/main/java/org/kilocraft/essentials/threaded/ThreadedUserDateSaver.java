9a687740b6ed851b1aa103b1a7b0c9084aa56e6c
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2020-01-03 03:58:43 +0330

    love this one

--- src/main/java/org/kilocraft/essentials/threaded/ThreadedUserDateSaver.java
+++ src/main/java/org/kilocraft/essentials/threaded/ThreadedUserDateSaver.java
@@ -30,7 +30,6 @@ public class ThreadedUserDateSaver implements KiloThread, Runnable {
                 e.printStackTrace();
             }
         }
-
         logger.info("Saved the users data!");
     }
 
