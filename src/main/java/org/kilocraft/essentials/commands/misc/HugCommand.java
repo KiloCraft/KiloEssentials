6d53fb5a04f162c929494656109c55b97f2e188a
Hans Kristian <hanskristianom@gmail.com>
2020-05-08 00:23:49 +0200

    Started work on HugCommand

--- src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
@@ -2,37 +2,42 @@ package org.kilocraft.essentials.commands.misc;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.EntitySelector;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import org.kilocraft.essentials.api.command.ArgumentCompletions;
 import org.kilocraft.essentials.api.command.EssentialCommand;
+import org.kilocraft.essentials.api.user.CommandSourceUser;
 import org.kilocraft.essentials.chat.KiloChat;
 import org.kilocraft.essentials.chat.LangText;
+import org.kilocraft.essentials.chat.ServerChat;
 
 import static net.minecraft.command.arguments.EntityArgumentType.getPlayer;
 import static net.minecraft.command.arguments.EntityArgumentType.player;
 
 public class HugCommand extends EssentialCommand {
-
     public HugCommand() {
         super("hug");
     }
 
+    @Override
     public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        RequiredArgumentBuilder<ServerCommandSource, EntitySelector> target = argument("target", player())
-                .suggests(ArgumentCompletions::allPlayers)
-                .executes(context -> execute(context.getSource(), getPlayer(context, "target")));
-
-        argumentBuilder.executes(context -> execute(context.getSource(), context.getSource().getPlayer()));
-        commandNode.addChild(target.build());
+        this.argumentBuilder.executes(this::execute);
     }
 
-    private static int execute(ServerCommandSource source, ServerPlayerEntity player) {
-        KiloChat.sendMessageTo(player, LangText.get(true, "command.hug.self"));
+    private int execute(CommandContext<ServerCommandSource> ctx) {
+        CommandSourceUser src = this.getServerUser(ctx);
+        src.sendLangMessage("command.hug.self");
+        src.asPlayer().playSound(
+                SoundEvents.ENTITY_ENDER_DRAGON_DEATH,
+                SoundCategory.MASTER,
+                1,
+                1);
         return 1;
     }
-
-
-
 }
