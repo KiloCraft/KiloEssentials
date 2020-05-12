b3554866251cb352780e7233456bce209243d348
Hans Kristian <hanskristianom@gmail.com>
2020-05-12 15:42:30 +0200

    New check player method. Will now check all players in-game then do the box check.

--- src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
@@ -3,14 +3,19 @@ package org.kilocraft.essentials.commands.misc;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+
 import org.kilocraft.essentials.api.command.EssentialCommand;
 import org.kilocraft.essentials.api.user.OnlineUser;
+
 import java.util.ArrayList;
 
 
@@ -31,15 +36,8 @@ public class HugCommand extends EssentialCommand {
         OnlineUser src = this.getOnlineUser(ctx);
         for (ServerPlayerEntity player : this.server.getPlayerManager().getPlayerList()) {
             OnlineUser target = getOnlineUser(player);
-            Box eBox = new Box(src.asPlayer().getBlockPos());
-            eBox = eBox.expand(2, 2, 2);
 
             if (!target.equals(src)) {
-
-                if (!eBox.contains(Vec3d.ofCenter(player.getBlockPos()))) {
-                    src.sendLangMessage("command.hug.notclose");
-                    return FAILED;
-                }
 
                 if (src.asPlayer().totalExperience < 16) {
                     src.sendLangMessage("command.hug.xp");
@@ -47,28 +45,21 @@ public class HugCommand extends EssentialCommand {
                 }
 
                 validTargets.add(player);
-
             }
         }
-        OnlineUser mainTarget = getOnlineUser(getClosest(validTargets, src));
-        src.asPlayer().addExperience(-16);
-        mainTarget.asPlayer().addExperience(8);
-
-        mainTarget.sendLangMessage("command.hug.message", src.getFormattedDisplayName());
-
-        mainTarget.asPlayer().playSound(
-                SoundEvents.ENTITY_FIREWORK_ROCKET_TWINKLE,
-                SoundCategory.MASTER,
-                1,
-                1
-        );
+        getClosest(validTargets, src);
         return SUCCESS;
     }
 
-    private ServerPlayerEntity getClosest(ArrayList<ServerPlayerEntity> validTargets, OnlineUser src) {
+    private int getClosest(ArrayList<ServerPlayerEntity> validTargets, OnlineUser src) {
+        Box eBox = new Box(src.asPlayer().getBlockPos());
+        eBox = eBox.expand(2, 2, 2);
+
         double srcLocation = src.asPlayer().getX();
         double distance = Math.abs(validTargets.get(0).getX() - srcLocation);
+
         int id = 0;
+
         for(int c = 1; c < validTargets.size(); c++){
             double cDistance = Math.abs(validTargets.get(c).getX() - srcLocation);
             if(cDistance < distance) {
@@ -76,6 +67,24 @@ public class HugCommand extends EssentialCommand {
                 distance = cDistance;
             }
         }
-        return validTargets.get(id);
+
+        if (!eBox.contains(Vec3d.ofCenter(validTargets.get(id).getBlockPos()))) {
+            src.sendLangMessage("command.hug.notclose");
+            return FAILED;
+        }
+
+        OnlineUser mainTarget  = getOnlineUser(validTargets.get(id));
+
+        src.asPlayer().addExperience(-16);
+
+        mainTarget.asPlayer().addExperience(8);
+        mainTarget.sendLangMessage("command.hug.message", src.getFormattedDisplayName());
+        mainTarget.asPlayer().playSound(
+                SoundEvents.ENTITY_FIREWORK_ROCKET_TWINKLE,
+                SoundCategory.MASTER,
+                1,
+                1
+        );
+        return SUCCESS;
     }
 }
