cc7d89e0a0e9776c9c43fd5174aa7a6cdf9f31fd
CODY_AI <cody_ai@outlook.com>
2020-05-13 20:27:30 +0430

    Fixed a few more things with the HugCommand

--- src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/misc/HugCommand.java
@@ -18,8 +18,6 @@ import org.kilocraft.essentials.api.user.OnlineUser;
 
 import java.util.ArrayList;
 
-
-
 public class HugCommand extends EssentialCommand {
     public HugCommand() {
         super("hug");
@@ -27,8 +25,7 @@ public class HugCommand extends EssentialCommand {
 
     @Override
     public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        this.argumentBuilder
-                .executes(this::execute);
+        this.argumentBuilder.executes(this::execute);
     }
 
     private int execute(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
@@ -38,9 +35,8 @@ public class HugCommand extends EssentialCommand {
             OnlineUser target = getOnlineUser(player);
 
             if (!target.equals(src)) {
-
                 if (src.asPlayer().totalExperience < 16) {
-                    src.sendLangMessage("command.hug.no_player_nearby");
+                    src.sendLangMessage("command.hug.xp");
                     return FAILED;
                 }
 
@@ -49,7 +45,7 @@ public class HugCommand extends EssentialCommand {
         }
 
         if (validTargets.isEmpty()) {
-            src.sendLangError("");
+            src.sendLangError("command.hug.no_player_nearby");
             return FAILED;
         }
 
@@ -69,25 +65,19 @@ public class HugCommand extends EssentialCommand {
 
         mainTarget.addExperience(8);
         onlineTarget.sendLangMessage("command.hug.message", src.getFormattedDisplayName());
-        mainTarget.playSound(
-                SoundEvents.ENTITY_FIREWORK_ROCKET_TWINKLE,
-                SoundCategory.MASTER,
-                1,
-                1
-        );
+        mainTarget.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_TWINKLE, SoundCategory.MASTER, 1, 1);
         return SUCCESS;
     }
 
     private ServerPlayerEntity getClosest(ArrayList<ServerPlayerEntity> validTargets, OnlineUser src) {
-
         double srcLocation = src.asPlayer().getX();
         double distance = Math.abs(validTargets.get(0).getX() - srcLocation);
 
         int id = 0;
 
-        for(int c = 1; c < validTargets.size(); c++){
+        for (int c = 1; c < validTargets.size(); c++) {
             double cDistance = Math.abs(validTargets.get(c).getX() - srcLocation);
-            if(cDistance < distance) {
+            if (cDistance < distance) {
                 id = c;
                 distance = cDistance;
             }
