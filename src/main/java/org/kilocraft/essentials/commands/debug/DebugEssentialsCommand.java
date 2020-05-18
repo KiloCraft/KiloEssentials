5d4cc42fd3825ed53f7b9c554f08f3786a607ed7
CODY_AI <cody_ai@outlook.com>
2020-05-18 05:46:08 +0430

    Update DebugEssentialsCommand.java

--- src/main/java/org/kilocraft/essentials/commands/debug/DebugEssentialsCommand.java
+++ src/main/java/org/kilocraft/essentials/commands/debug/DebugEssentialsCommand.java
@@ -1,38 +1,11 @@
 package org.kilocraft.essentials.commands.debug;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import net.minecraft.SharedConstants;
-import net.minecraft.command.arguments.NbtCompoundTagArgumentType;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.Formatting;
 import org.kilocraft.essentials.KiloDebugUtils;
 import org.kilocraft.essentials.api.command.EssentialCommand;
-import org.kilocraft.essentials.api.text.TextInput;
-import org.kilocraft.essentials.api.util.Cached;
-import org.kilocraft.essentials.chat.KiloChat;
-import org.kilocraft.essentials.config.KiloConfig;
-import org.kilocraft.essentials.util.CacheManager;
-import org.kilocraft.essentials.util.text.Pager;
-import org.kilocraft.essentials.util.text.Texter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class DebugEssentialsCommand extends EssentialCommand {
     public DebugEssentialsCommand() {
