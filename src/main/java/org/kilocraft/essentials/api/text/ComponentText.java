d40c89c6f47be533465b3f4aed80246602a419e9
DrexHD <nicknamedrex@gmail.com>
2020-11-06 19:24:15 +0100

    improve color stripping regex

--- src/main/java/org/kilocraft/essentials/api/text/ComponentText.java
+++ src/main/java/org/kilocraft/essentials/api/text/ComponentText.java
@@ -125,7 +125,7 @@ public class ComponentText {
     }
 
     public static String stripColor(String s) {
-        return s.replaceAll("<\\/?((color:#?\\w+)|black|dark_blue|dark_green|dark_aqua|dark_red|dark_purple|gold|gray|dark_gray|blue|green|aqua|red|light_purple|yellow|white)>", "");
+        return s.replaceAll("<\\/?((color:#\\w+)|(#\\w+)|((color:)?(black|dark_blue|dark_green|dark_aqua|dark_red|dark_purple|gold|gray|dark_gray|blue|green|aqua|red|light_purple|yellow|white)))>", "");
     }
 
     public static String stripFormatting(String s) {
