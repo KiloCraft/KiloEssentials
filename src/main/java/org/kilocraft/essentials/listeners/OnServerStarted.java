288306a7f8920e312c41e2dd13c472ea6fea76a6
DrexHD <nicknamedrex@gmail.com>
2020-12-11 00:32:42 +0100

    make sure lp is loaded

--- src/main/java/org/kilocraft/essentials/listeners/OnServerStarted.java
+++ src/main/java/org/kilocraft/essentials/listeners/OnServerStarted.java
@@ -1,5 +1,6 @@
 package org.kilocraft.essentials.listeners;
 
+import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import org.jetbrains.annotations.NotNull;
 import org.kilocraft.essentials.api.event.EventHandler;
@@ -9,6 +10,13 @@ public class OnServerStarted implements EventHandler<ServerStartedEvent> {
 
     @Override
     public void handle(@NotNull ServerStartedEvent event) {
-        LuckPermsListener listener = new LuckPermsListener(LuckPermsProvider.get());
+        LuckPerms luckPerms = null;
+        while (luckPerms == null) {
+            try {
+                luckPerms = LuckPermsProvider.get();
+            } catch (IllegalStateException ignored) {}
+        }
+        LuckPermsListener listener = new LuckPermsListener(luckPerms);
+
     }
 }
