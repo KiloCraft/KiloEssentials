a9d6e2cb1897b2671b89ceed0489953b00b6aa2c
DrexHD <nicknamedrex@gmail.com>
2021-01-18 18:46:35 +0100

    improve entity interact command

--- src/main/java/org/kilocraft/essentials/mixin/EntityMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/EntityMixin.java
@@ -12,15 +12,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class EntityMixin {
 
     String command = "";
+    String leftCommand = "";
+    String rightCommand = "";
 
-    @Inject(method = "fromTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getList(Ljava/lang/String;I)Lnet/minecraft/nbt/ListTag;"))
+    @Inject(method = "fromTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getList(Ljava/lang/String;I)Lnet/minecraft/nbt/ListTag;", ordinal = 0))
     public void addCommandFromTag(CompoundTag tag, CallbackInfo ci) {
         if (tag.contains("command")) this.command = tag.getString("command");
+        if (tag.contains("leftCommand")) this.leftCommand = tag.getString("leftCommand");
+        if (tag.contains("rightCommand")) this.rightCommand = tag.getString("rightCommand");
     }
 
     @Inject(method = "toTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;put(Ljava/lang/String;Lnet/minecraft/nbt/Tag;)Lnet/minecraft/nbt/Tag;"))
     public void addCommandToTag(CompoundTag tag, CallbackInfoReturnable<CompoundTag> cir) {
         if (!this.command.equals("")) tag.putString("command", this.command);
+        if (!this.leftCommand.equals("")) tag.putString("leftCommand", this.leftCommand);
+        if (!this.rightCommand.equals("")) tag.putString("rightCommand", this.rightCommand);
     }
 
 }
--- src/main/java/org/kilocraft/essentials/mixin/events/MixinServerPlayNetworkHandler$PlayerEvents.java
+++ src/main/java/org/kilocraft/essentials/mixin/events/MixinServerPlayNetworkHandler$PlayerEvents.java
@@ -11,33 +11,22 @@ import net.minecraft.network.MessageType;
 import net.minecraft.network.NetworkThreadUtils;
 import net.minecraft.network.listener.ServerPlayPacketListener;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Util;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import org.kilocraft.essentials.api.KiloServer;
-import org.kilocraft.essentials.api.event.player.PlayerInteractBlockEvent;
 import org.kilocraft.essentials.api.event.player.PlayerInteractItemStartEvent;
 import org.kilocraft.essentials.commands.CommandUtils;
 import org.kilocraft.essentials.events.player.PlayerClientCommandEventImpl;
 import org.kilocraft.essentials.events.player.PlayerDisconnectEventImpl;
-import org.kilocraft.essentials.events.player.PlayerInteractBlockEventImpl;
 import org.kilocraft.essentials.events.player.PlayerInteractItemStartEventImpl;
 import org.kilocraft.essentials.util.registry.RegistryUtils;
 import org.spongepowered.asm.mixin.Final;
@@ -55,7 +44,7 @@ public abstract class MixinServerPlayNetworkHandler$PlayerEvents {
 
     @Shadow
     public ServerPlayerEntity player;
-    int interactions = 0;
+    long lastInteraction = 0;
     @Shadow
     @Final
     private MinecraftServer server;
@@ -106,55 +95,32 @@ public abstract class MixinServerPlayNetworkHandler$PlayerEvents {
         }
     }
 
-    @Inject(method = "onPlayerInteractBlock", cancellable = true,
-            at = @At(value = "HEAD", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;onPlayerInteractBlock(Lnet/minecraft/network/packet/c2s/play/PlayerInteractBlockC2SPacket;)V"))
-    private void ke$modify$onInteractBlock(PlayerInteractBlockC2SPacket playerInteractBlockC2SPacket, CallbackInfo ci) {
-        ci.cancel();
-        NetworkThreadUtils.forceMainThread(playerInteractBlockC2SPacket, player.networkHandler, this.player.getServerWorld());
-        ServerWorld serverWorld = this.player.getServerWorld();
-        Hand hand = playerInteractBlockC2SPacket.getHand();
-        ItemStack itemStack = this.player.getStackInHand(hand);
-        BlockHitResult blockHitResult = playerInteractBlockC2SPacket.getBlockHitResult();
-        BlockPos blockPos = blockHitResult.getBlockPos();
-        Direction direction = blockHitResult.getSide();
-        this.player.updateLastActionTime();
-
-        PlayerInteractBlockEvent event = new PlayerInteractBlockEventImpl(player, playerInteractBlockC2SPacket.getBlockHitResult(), hand);
-        KiloServer.getServer().triggerEvent(event);
-        if (!event.isCancelled()) {
-            if (blockPos.getY() < this.server.getOverworld().getTopHeightLimit()) {
-                if (this.requestedTeleportPos == null && this.player.squaredDistanceTo((double) blockPos.getX() + 0.5D, (double) blockPos.getY() + 0.5D, (double) blockPos.getZ() + 0.5D) < 64.0D && serverWorld.canPlayerModifyAt(this.player, blockPos)) {
-                    ActionResult actionResult = this.player.interactionManager.interactBlock(this.player, serverWorld, itemStack, hand, blockHitResult);
-                    if (direction == Direction.UP && actionResult != ActionResult.SUCCESS && blockPos.getY() >= this.server.getOverworld().getTopHeightLimit() - 1 && shouldContinueUsingItem(this.player, itemStack)) {
-                        Text text = (new TranslatableText("build.tooHigh", this.server.getOverworld().getTopHeightLimit())).formatted(Formatting.RED);
-                        this.player.networkHandler.sendPacket(new GameMessageS2CPacket(text, MessageType.GAME_INFO, Util.NIL_UUID));
-                    } else if (actionResult.shouldSwingHand()) {
-                        this.player.swingHand(hand, true);
-                    }
-                }
-            } else {
-                Text text = (new TranslatableText("build.tooHigh", this.server.getOverworld().getTopHeightLimit())).formatted(Formatting.RED);
-                this.player.networkHandler.sendPacket(new GameMessageS2CPacket(text, MessageType.GAME_INFO, Util.NIL_UUID));
-            }
-        }
-
-        this.player.networkHandler.sendPacket(new BlockUpdateS2CPacket(serverWorld, blockPos));
-        this.player.networkHandler.sendPacket(new BlockUpdateS2CPacket(serverWorld, blockPos.offset(direction)));
-    }
-
     @Inject(method = "onPlayerInteractEntity", at = @At(value = "HEAD"))
     private void ke$onPlayerInteractEntity(PlayerInteractEntityC2SPacket playerInteractEntityC2SPacket, CallbackInfo ci) {
-        interactions++;
-        if (interactions % 2 == 0) return;
+        if (lastInteraction + 50 > Util.getMeasuringTimeMs()) return;
         NetworkThreadUtils.forceMainThread(playerInteractEntityC2SPacket, (ServerPlayNetworkHandler) (Object) this, this.player.getServerWorld());
         ServerWorld serverWorld = this.player.getServerWorld();
         Entity entity = playerInteractEntityC2SPacket.getEntity(serverWorld);
-        EntityDataObject entityDataObject = new EntityDataObject(entity);
-        CompoundTag tag = entityDataObject.getTag();
-        String command = tag.getString("command");
-        if (!command.equals("")) {
-            CommandUtils.runCommandWithFormatting(this.player.getCommandSource(), command);
+        if (entity != null) {
+            EntityDataObject entityDataObject = new EntityDataObject(entity);
+            CompoundTag tag = entityDataObject.getTag();
+            String command = tag.getString("command");
+            String specificCommand = playerInteractEntityC2SPacket.getType() ==
+                    PlayerInteractEntityC2SPacket.InteractionType.ATTACK ?
+                    tag.getString("leftCommand") :
+                    tag.getString("rightCommand");
+            boolean success = false;
+            if (!command.equals("")) {
+                CommandUtils.runCommandWithFormatting(this.player.getCommandSource(), command);
+                success = true;
+            }
+            if (!specificCommand.equals("")) {
+                CommandUtils.runCommandWithFormatting(this.player.getCommandSource(), specificCommand);
+                success = true;
+            }
+            if (success) this.player.swingHand(playerInteractEntityC2SPacket.getHand(), true);
         }
+        lastInteraction = Util.getMeasuringTimeMs();
     }
 
 
