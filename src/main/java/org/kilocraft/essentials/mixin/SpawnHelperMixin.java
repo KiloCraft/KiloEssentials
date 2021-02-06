0fcc2afc5e46633f4406ae23e1da4e0b61a7efd4
DrexHD <nicknamedrex@gmail.com>
2021-02-07 00:53:34 +0100

    only spawn entities in ticking chunks

--- src/main/java/org/kilocraft/essentials/mixin/SpawnHelperMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/SpawnHelperMixin.java
@@ -1,32 +1,28 @@
 package org.kilocraft.essentials.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.chunk.Chunk;
 import org.kilocraft.essentials.util.DataTracker;
 import org.kilocraft.essentials.util.settings.ServerSettings;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerChunkManager.class)
-public class ServerChunkManagerMixin {
+@Mixin(SpawnHelper.class)
+public abstract class SpawnHelperMixin {
 
-    @Shadow
-    @Final
-    private ServerWorld world;
-
-    @Inject(method = "shouldTickChunk", at = @At(value = "HEAD"), cancellable = true)
-    public void shouldSpawnEntities(ChunkPos chunkPos, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "isAcceptableSpawnPosition", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;getPos()Lnet/minecraft/util/math/ChunkPos;"), cancellable = true)
+    private static void shouldSpawnEntities(ServerWorld serverWorld, Chunk chunk, BlockPos.Mutable mutable, double d, CallbackInfoReturnable<Boolean> cir) {
         int tickDistance = ServerSettings.TICK_DISTANCE.getValue();
         if (tickDistance != -1) {
-            Entity player = this.world.getClosestPlayer(chunkPos.getStartX() + 8, 128, chunkPos.getStartZ() + 8, -1.0D, false);
+            Entity player = serverWorld.getClosestPlayer(chunk.getPos().getStartX() + 8, 128, chunk.getPos().getStartZ() + 8, -1.0D, false);
             if (player != null) {
-                if (chunkPos.method_24022(player.getChunkPos()) > tickDistance) {
+                if (chunk.getPos().method_24022(player.getChunkPos()) > tickDistance) {
                     DataTracker.add(DataTracker.cSpawnAttempts);
                     cir.setReturnValue(false);
                     return;
@@ -35,5 +31,4 @@ public class ServerChunkManagerMixin {
         }
         DataTracker.add(DataTracker.spawnAttempts);
     }
-
 }
