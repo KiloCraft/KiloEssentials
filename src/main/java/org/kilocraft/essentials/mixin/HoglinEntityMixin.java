2dac26d184a8db291f54cfb7d2ae4cabd4620bbe
DrexHD <nicknamedrex@gmail.com>
2021-03-25 23:32:41 +0100

    bug fix, dupe fix

--- src/main/java/org/kilocraft/essentials/mixin/HoglinEntityMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/HoglinEntityMixin.java
@@ -1,23 +1,30 @@
 package org.kilocraft.essentials.mixin;
 
 import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.HoglinEntity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
 import org.kilocraft.essentials.api.KiloEssentials;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(HoglinEntity.class)
-public class HoglinEntityMixin {
+public abstract class HoglinEntityMixin extends PassiveEntity {
+
+    protected HoglinEntityMixin(EntityType<? extends PassiveEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/AnimalEntity;initialize(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/LocalDifficulty;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/entity/EntityData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/entity/EntityData;"))
     public EntityData onInitialize(AnimalEntity animalEntity, ServerWorldAccess serverWorldAccess, LocalDifficulty localDifficulty, SpawnReason spawnReason, EntityData entityData, CompoundTag compoundTag) {
-        if (!(entityData instanceof HoglinEntity)) {
+        if (entityData != null && !(entityData instanceof HoglinEntity) && !(entityData instanceof PassiveData)) {
             KiloEssentials.getLogger().warn("There was an error initializing " + this.toString());
             KiloEssentials.getLogger().warn("World: " + serverWorldAccess.toServerWorld().toString());
             KiloEssentials.getLogger().warn("Spawnreason: " + spawnReason.toString());
@@ -25,7 +32,7 @@ public class HoglinEntityMixin {
             KiloEssentials.getLogger().warn("CompoundTag: " + compoundTag);
             entityData = null;
         }
-        return animalEntity.initialize(serverWorldAccess, localDifficulty, spawnReason, entityData, compoundTag);
+        return super.initialize(serverWorldAccess, localDifficulty, spawnReason, entityData, compoundTag);
     }
 
 }
--- src/main/java/org/kilocraft/essentials/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,6 +3,10 @@ package org.kilocraft.essentials.mixin;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.NetworkThreadUtils;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import net.minecraft.network.packet.c2s.play.UpdateSignC2SPacket;
@@ -25,6 +29,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
@@ -57,7 +62,7 @@ public abstract class ServerPlayNetworkHandlerMixin {
 //    }
 
     @Inject(
-            method = "onSignUpdate", cancellable = true,
+            method = "onSignUpdate(Lnet/minecraft/network/packet/c2s/play/UpdateSignC2SPacket;)V", cancellable = true,
             at = @At(
                     value = "HEAD",
                     target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;onSignUpdate(Lnet/minecraft/network/packet/c2s/play/UpdateSignC2SPacket;)V"
@@ -93,6 +98,26 @@ public abstract class ServerPlayNetworkHandlerMixin {
             signBlockEntity.markDirty();
             serverWorld.updateListeners(blockPos, blockState, blockState, 3);
         }
+    }
+
+    @Redirect(method = "onBookUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getList(Ljava/lang/String;I)Lnet/minecraft/nbt/ListTag;"))
+    public ListTag weDontNeedThesePages(CompoundTag compoundTag, String string, int i) {
+        ListTag listTag = compoundTag.getList("pages", 8);
+        boolean dupeAttempt = false;
+        for (int j = 0; j < listTag.size(); j++) {
+            Tag tag = listTag.get(j);
+            if (tag instanceof StringTag) {
+                StringTag stringTag = (StringTag) tag;
+                final String s = stringTag.asString();
+                if (s.length() > 300) {
+                    stringTag = StringTag.of(s.substring(0, 300));
+                    listTag.set(j, stringTag);
+                    dupeAttempt = true;
+                }
+            }
+        }
+        if (dupeAttempt) KiloEssentials.getLogger().warn(player.getEntityName() + " attempted to dupe!");
+        return listTag;
     }
 
 }
