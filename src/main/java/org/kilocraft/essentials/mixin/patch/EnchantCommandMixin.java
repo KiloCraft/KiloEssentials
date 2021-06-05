9e92b78e6ab4d3fae5aa7024b998c174bc29d6a2
DrexHD <nicknamedrex@gmail.com>
2021-06-05 14:22:38 +0200

    change: Removed level limit for enchantment command

---
+++ src/main/java/org/kilocraft/essentials/mixin/patch/EnchantCommandMixin.java
@@ -0,0 +1,23 @@
+package org.kilocraft.essentials.mixin.patch;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.EnchantCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EnchantCommand.class)
+public class EnchantCommandMixin {
+
+    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;getMaxLevel()I"))
+    private static int noMaxLevel(Enchantment enchantment) {
+        return Integer.MAX_VALUE;
+    }
+
+
+    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;isAcceptableItem(Lnet/minecraft/item/ItemStack;)Z"))
+    private static boolean alwaysCompatible$2(Enchantment enchantment, ItemStack itemStack) {
+        return true;
+    }
+}
