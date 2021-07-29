2df9914189f3dbbfa9973883edca6daad6ccb2e3
DrexHD <nicknamedrex@gmail.com>
2021-07-30 00:06:36 +0200

    add: Packet limit
    This will kick any player that sends more than 500 packets per second

---
+++ src/main/java/org/kilocraft/essentials/mixin/patch/ClientConnectionMixin.java
@@ -0,0 +1,45 @@
+package org.kilocraft.essentials.mixin.patch;
+
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.Packet;
+import net.minecraft.network.listener.PacketListener;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.LiteralText;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.kilocraft.essentials.api.KiloEssentials;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin {
+
+    private static final long MAX_PACKETS_PER_SECOND = 500;
+    private int packetsSinceLastInterval = 0;
+    private long lastInterval = System.currentTimeMillis();
+
+    @Shadow
+    private PacketListener packetListener;
+
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
+        if (packetListener instanceof ServerPlayNetworkHandler networkHandler) {
+            if ((System.currentTimeMillis() - lastInterval) > 1000) {
+                packetsSinceLastInterval = 0;
+                lastInterval = System.currentTimeMillis();
+            }
+            if (packetsSinceLastInterval >= MAX_PACKETS_PER_SECOND) {
+                networkHandler.disconnect(new LiteralText("Too Many Packets!"));
+            }
+            packetsSinceLastInterval++;
+        }
+    }
+
+    @Inject(method = "exceptionCaught", at = @At("HEAD"))
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable, CallbackInfo cb) {
+        KiloEssentials.getLogger().info(ExceptionUtils.getStackTrace(throwable));
+    }
+}
