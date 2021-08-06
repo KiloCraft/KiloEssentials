c3032e6f2087d2a150bc78308520541e7d482d13
DrexHD <nicknamedrex@gmail.com>
2021-08-06 23:26:04 +0200

    add: Papers maps shouldn't load chunks patch
    https://github.com/PaperMC/Paper/blob/master/patches/server/0451-Maps-shouldn-t-load-chunks.patch

---
+++ src/main/java/org/kilocraft/essentials/mixin/patch/FilledMapItemMixinPatch.java
@@ -0,0 +1,27 @@
+package org.kilocraft.essentials.mixin.patch;
+
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FilledMapItem.class)
+public class FilledMapItemMixinPatch {
+
+    @Redirect(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getWorldChunk(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/world/chunk/WorldChunk;"))
+    public WorldChunk getChunkIfLoaded(World world, BlockPos pos) {
+        //Maps shouldn't load chunks
+        return world.getChunkManager().getWorldChunk(ChunkSectionPos.getSectionCoord(pos.getX()), ChunkSectionPos.getSectionCoord(pos.getZ()));
+    }
+
+    @Redirect(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;isEmpty()Z"))
+    public boolean validateChunkNotNull(WorldChunk worldChunk) {
+        //worldChunk may be null, because of the above redirect
+        return worldChunk != null && !worldChunk.isEmpty();
+    }
+
+}
