6f837716691533bbfeffb5f9c6aec0c0962ee069
DrexHD <nicknamedrex@gmail.com>
2021-08-12 14:13:58 +0200

    add: Players only move to spawn on first join

--- src/main/java/org/kilocraft/essentials/mixin/patch/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/patch/ServerPlayNetworkHandlerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayNetworkHandler.class)
-public class ServerPlayNetworkHandlerMixin {
+public abstract class ServerPlayNetworkHandlerMixin {
 
     @Unique
     private boolean loggedIn = false;
