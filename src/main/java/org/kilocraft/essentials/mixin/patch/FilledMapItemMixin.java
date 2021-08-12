ec2688e6087cf8ea80864b53df2b27dc800f88ba
DrexHD <nicknamedrex@gmail.com>
2021-08-12 18:05:23 +0200

    add: Map performance patch
    source: https://github.com/Wesley1808/ServerCore-Fabric/blob/1.17.1/src/main/java/org/provim/servercore/mixin/performance/MapStateMixin.java

--- src/main/java/org/kilocraft/essentials/mixin/patch/FilledMapItemMixin.java
+++ src/main/java/org/kilocraft/essentials/mixin/patch/FilledMapItemMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
+import org.kilocraft.essentials.patch.ChunkManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,20 +13,18 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(FilledMapItem.class)
 public abstract class FilledMapItemMixin {
 
+    /**
+     * Stop maps from loading chunks.
+     */
+
     @Redirect(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getWorldChunk(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/world/chunk/WorldChunk;"))
-    public WorldChunk getChunkIfLoaded(World world, BlockPos pos) {
-        //Maps shouldn't load chunks
-        return world.getChunkManager().getWorldChunk(ChunkSectionPos.getSectionCoord(pos.getX()), ChunkSectionPos.getSectionCoord(pos.getZ()));
+    private WorldChunk onlyUpdateIfLoaded(World world, BlockPos pos) {
+        return (WorldChunk) ChunkManager.getChunkIfVisible(world, pos);
     }
 
     @Redirect(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;isEmpty()Z"))
-    public boolean validateChunkNotNull(WorldChunk worldChunk) {
-        //worldChunk may be null, because of the above redirect
-        if (worldChunk == null) {
-            return true;
-        } else {
-            return worldChunk.isEmpty();
-        }
+    private boolean checkNull(WorldChunk worldChunk) {
+        return worldChunk == null || worldChunk.isEmpty();
     }
 
 }
